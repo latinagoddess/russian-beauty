c3e927a6fb5a1290467248029a5235fb5e029fb4
unknown <latinagoddess72@gmail.com>
2025-10-24 03:19:26 -0700

    fixing cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  //output: "export",
   trailingSlash: true, // <--- SET THIS TO TRUE
   images: {
     unoptimized: true, // Required for static export
@@ -9,7 +9,7 @@ const nextConfig = {
     NEXT_PUBLIC_RESEND_API_KEY: "re_AadsE7Kq_KayfnTGAwP4ZjQKHVZFNVNtJ",
     NEXT_PUBLIC_EMAIL: "foreignprincess888@gmail.com",
     NEXT_PUBLIC_PHONE_NUMBER: "+1 416-618-9575",
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6LcR63QqAAAAABAYbLlmwvWuKmWMi4W7EA7d3Ii1",
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6LdQLvUrAAAAAG5884zJPfGX62Ze4_MiJ_QFq1ju",
     NEXT_PUBLIC_SPAIN_SITE: "http://www.spain-ppr.com/",
     NEXT_PUBLIC_BRAZIL_SITE: "http://the-affiliatessp.com/",
     NEXT_PUBLIC_CERB_SITE: "http://vip-cerb.com/",
