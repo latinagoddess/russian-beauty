95578f74d949688fed684feb3b9ae4f80c4c145f
unknown <latinagoddess72@gmail.com>
2025-10-24 02:37:55 -0700

    fixed github push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   trailingSlash: true, // <--- SET THIS TO TRUE
   images: {
     unoptimized: true, // Required for static export
